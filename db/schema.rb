7ec50c5af1efb5914bb965b3e9ba0192d9cced4e
renatomno <xrenatomno@gmail.com>
2022-10-12 16:11:41 -0300

    :card_file_box: add Users table and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_05_232232) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_182805) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.string "cpf"
+    t.string "email"
+    t.date "birthdate"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
